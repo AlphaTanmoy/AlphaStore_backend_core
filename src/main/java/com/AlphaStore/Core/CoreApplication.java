ab6f8530194101ab1cdf0106fd8a83eea7714791
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-09 22:34:55 +0530

    basefile added

--- src/main/java/com/AlphaStore/Core/CoreApplication.java
+++ src/main/java/com/AlphaStore/Core/CoreApplication.java
@@ -3,9 +3,14 @@ package com.AlphaStore.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {
+		"com.AlphaStore.Core",
+		"com.AlphaStore.Utils"
+})
 public class CoreApplication {
 
 	public static void main(String[] args) {
