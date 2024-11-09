8997d90d1b6d6fe12fb8de657f08bab8f7ba6ff6
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 01:18:27 +0530

    changes in util

--- src/main/kotlin/com/AlphaStore/Core/CoreApplication.kt
+++ src/main/kotlin/com/AlphaStore/Core/CoreApplication.kt
@@ -2,8 +2,18 @@ package com.alphaStore.Core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = [
+	"com.alphaStore.Core",
+	"com.alphaStore.Utils",
+	"com.alphaStore.Admin",
+	"com.alphaStore.Mail",
+	"com.alphaStore.Otp"
+])
 class CoreApplication
 
 fun main(args: Array<String>) {
